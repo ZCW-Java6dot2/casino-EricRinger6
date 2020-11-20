7cfbe42bcebfcd3f04eaef3300909d47eec5d6a0
DavidToribio1 <dtmuzik1@gmail.com>
2020-11-19 20:41:45 -0500

    craps need test

--- src/main/java/io/zipcoder/casino/craps/Craps.java
+++ src/main/java/io/zipcoder/casino/craps/Craps.java
@@ -1,3 +1,4 @@
+package io.zipcoder.casino.craps;
 import io.zipcoder.casino.core.DiceGame;
 import io.zipcoder.casino.core.GamblingGame;
 import io.zipcoder.casino.core.Player;
@@ -23,103 +24,101 @@ public class Craps extends DiceGame implements GamblingGame {
         this.console = console;
         diceGame = new DiceGame();
     }
-    //Check player balance. Player must have at least 1 chip to buy in (Actual Game)
+    public Craps()
+    {
+    }
+    //Game start
     public void gameOn() {
         firstRoll = true;
         while(firstRoll) {
-            playerHasMoney();
             extraRoll = true;
+            playerHasMoney();
             rollBet();
             while(extraRoll) {
                 extraRollBet();
             }
         }
     }
-    //getting value rolling die and storing in new array
+    //getting the value of a roll and storing in dieArr
     public void getDie() {
         dieRolls = diceGame.tossAndList(2);
         extraDieVal = dieRolls.get(0) + dieRolls.get(1);
         dieArr.add(extraDieVal);
     }
-    //First roll and determines if your first win or loss
     public void rollBet() {
         getDie();
         if(dieArr.get(0) == 7 || dieArr.get(0) == 11) {
             console.println("You win on the first round with a " + dieArr.get(0).toString());
             youWin();
+            extraRoll = false;
             firstRoll = playAgainBoo();
         } else if (dieArr.get(0) == 2 || dieArr.get(0) == 3 || dieArr.get(0) == 12) {
             console.println("You lose on the first roll with a " + dieArr.get(0).toString());
             youLose();
+            extraRoll = false;
             firstRoll = playAgainBoo();
         }
     }
-    //the roll after first roll 2nd 3rd 4th roll if first value of die= most recent toss then win or if roll 7 you lose
-    public void extraRollBet() {
+    public Integer extraRollBet() {
+        Integer isTrue = 0;
         getDie();
         if(dieArr.get(0) == dieArr.get(dieArr.size() - 1)) {
+            isTrue = 1;
             console.println("Your first roll was a " + dieArr.get(0).toString() + ", you matched that roll to win in a later round.");
             youWin();
             extraRoll = false;
             firstRoll = playAgainBoo();
         } else if (dieArr.get(dieArr.size() - 1) == 7) {
+            isTrue = 0;
             console.println("Your first roll was a " + dieArr.get(0).toString() + " but you rolled a 7 in a later round.");
             youLose();
             extraRoll = false;
             firstRoll = playAgainBoo();
         }
+        return isTrue;
     }
-    //receiving input if you would like to play again
     public String playAgainStr() {
         String play = console.getStringInput("Would you like to play again?  (Y or N)");
         return play;
     }
-    //Takes value of playagainstStr and capitalizes and if they enter lower or upper case n becomes false if you press anything it is true
     public Boolean playAgainBoo() {
         String playStr = playAgainStr().toUpperCase();
         return !playStr.equals("N");
     }
-    //Checking if player has over $1 then check player bet
     public void playerHasMoney() {
         //hasMoney = 0;
         if(playerMoney >= 1) {
             //hasMoney = 1;
             checkPlayerBet();
-        } else {
+        } else if (playerMoney < 0) {
         }
     }
-    //Checks if player has more money than he is betting if not
     public void checkPlayerBet() {
         betAmt = getPlayerBet();
-        if(betAmt > playerMoney) {
-            firstRoll = false;
+        if(betAmt > player.getPlayerMoney()) {
             console.println("need more cash");
+            firstRoll = false;
         }
     }
-    //getter for collecting bet amount
     public Double getBetAmt() {
         return betAmt;
     }
-    //printer gets new bet
     public double getPlayerBet() {
         console.println("You currently have $" + player.getPlayerMoney());
         Double newBet = console.getDoubleInput("How much would you like to bet?");
         return newBet;
     }
-    //displays player balance
     public double displayPlayerWallet() {
         return player.getPlayerMoney();
     }
-    //sets money and adds money to the balance you bet and clears array
     public void youWin() {
         player.setPlayerMoney(player.getPlayerMoney() + getBetAmt());
         console.println("Congrats you won $" + getBetAmt());
         dieArr.clear();
     }
-    //sets bet amount and then subtracts from the balance and clears array
     public void youLose() {
         player.setPlayerMoney(player.getPlayerMoney() - getBetAmt());
-        console.println(" You Lose $" + getBetAmt());
+        console.println(" You Lost $" + getBetAmt());
         dieArr.clear();
     }
 }
--- src/test/java/io/zipcoder/casino/craps/CrapsTest.java
+++ src/test/java/io/zipcoder/casino/craps/CrapsTest.java
@@ -9,66 +9,19 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 
-public class CrapsTest {
-    ArrayList<Integer> testArr;
-    Craps craps = new Craps(); // ITS NULL RIGHT NOW if you solve this you can have morre test cases
+/*public class CrapsTest {
+   ArrayList<Integer> testArr;
+   Craps craps = new Craps(); // ITS NULL RIGHT NOW if you solve this you can have morre test cases
     DiceGame diceGame = new DiceGame();
     Console console = new Console(System.in, System.out);
-    //  Player player = new Player();
-    @Test
-    public void getDie() {
+      Player player = new Player();*/
+    //@Test
+   /* public void getDie() {
         ArrayList<Integer> dieArr = new ArrayList<Integer>();
         Integer dieNum = 2;
         craps.getDie();
         System.out.println(craps.dieArr);
-        //  Assert.assertTrue(dieArr.size() == dieNum);
-        //        Integer dieNum = 0;
-//        testArr = diceGame.tossAndList(2);
-//        dieNum = testArr.get(0) + testArr.get(1);
-//        boolean expected;
-//        if(dieNum > 1 && dieNum < 13) {
-//            expected = true;
-//        } else {
-//            expected = false;
-//
-    }
-  /*  @Test
-    void gameOn() {
-    }
-    @Test
-    void getDie() {
-    }
-    @Test
-    void rollBet() {
-    }
-    @Test
-    void extraRollBet() {
-    }
-    @Test
-    void playAgainStr() {
-    }
-    @Test
-    void playAgainBoo() {
-    }
-    @Test
-    void playerHasMoney() {
-    }
-    @Test
-    void checkPlayerBet() {
-    }
-    @Test
-    void getBetAmt() {
-    }
-    @Test
-    void getPlayerBet() {
-    }
-    @Test
-    void displayPlayerWallet() {
-    }
-    @Test
-    void youWin() {
-    }
-    @Test
-    void youLose() {
+          Assert.assertTrue(dieArr.size() == dieNum);
+               Integer dieNum = 0;
+       testArr = diceGame.tossAndList(2);
     }*/
-}
